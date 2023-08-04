b7d5db142285426565fd1a4c69aa32d8e2a0d352
Dodolight <amoussoudonan1@gmail.com>
2023-08-04 12:25:59 +0100

    Next bug fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  // reactStrictMode: unoptimized,
   images: {
-    domains: ["images.unsplash.com"],
+    loader: "imgix",
+    path: "/",
   },
 };
 
