06babdb47e67f5c3d50c1c51601f2065f856d84c
Dodolight <amoussoudonan1@gmail.com>
2023-08-18 18:23:01 +0100

    fix image bug

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   // reactStrictMode: unoptimized,
-  images: {
-    loader: "imgix",
-    path: "/",
-  },
+  reactStrictMode: false,
+  // images: {
+  //   unoptimized: true,
+  // },
 };
 
 module.exports = nextConfig;
